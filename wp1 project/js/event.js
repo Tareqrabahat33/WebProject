b53f95e01f5b570db9cbb4b7b6c742f7cb7053ab
hazimeh2005 <m.hazimeh2004@gmail.com>
2025-05-12 11:19:35 -0700

    added confimation event

---
+++ wp1 project/js/event.js
@@ -0,0 +1,8 @@
+ function showconfirm(){
+            let res=confirm("we will be able to access ur personal info, sure you want to procced");
+            if (res){
+                console.log("confirmed")
+            } else{
+                console.log("canceled")
+            }
+        }
